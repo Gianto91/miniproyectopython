a23dd9286ebc272708ca537b364f5e9e73957b44
Gian Peralta <gianperalta@Mac-mini-de-Gian.local>
2025-08-11 21:52:11 -0500

    Quinto commint

--- features/steps/steps_buscar_google.py
+++ features/steps/steps_buscar_google.py
@@ -1,5 +1,10 @@
+import tempfile
 from behave import given, when, then
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import allure
 from utils import tomar_captura
 
@@ -8,16 +13,15 @@ def tomar_captura(driver, nombre="captura"):
     driver.save_screenshot(screenshot_path)
     allure.attach.file(screenshot_path, name=nombre, attachment_type=allure.attachment_type.PNG)
 
-
 @given('que abro Google')
 def step_abrir_google(context):
-    from selenium import webdriver
-    from selenium.webdriver.chrome.service import Service
-    from selenium.webdriver.chrome.options import Options
-    from webdriver_manager.chrome import ChromeDriverManager
-
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
     chrome_options.add_argument("--start-maximized")
+
     service = Service(ChromeDriverManager().install())
     context.driver = webdriver.Chrome(service=service, options=chrome_options)
     context.driver.get("https://www.google.com")
--- helpers.py
+++ helpers.py
@@ -1,10 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import tempfile
 import allure
 
 def get_browser():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless=new")  # Modo headless
+    chrome_options.add_argument("--no-sandbox")  # Recomendado en entornos CI/CD
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
     driver.maximize_window()
     return driver
 
