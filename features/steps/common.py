97fcec0efb669936b3f0981e64ca22c38b57ef26
Gian Peralta <gianperalta@Mac-mini-de-Gian.local>
2025-08-11 21:29:10 -0500

    Cuarto commit con ejemplo Selenium + API

--- features/steps/common.py
+++ features/steps/common.py
@@ -5,7 +5,20 @@ import requests
 import allure
 
 def get_browser():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    # No uses --user-data-dir
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
     driver.maximize_window()
     return driver
 
